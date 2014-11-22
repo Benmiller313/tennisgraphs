4319ca1339fdaea47d6910e38a3f56f9bf7b4708
benmiller313 <benmiller313@gmail.com>
2014-11-21 23:19:34 -0700

    added logic to fetch matches from mongo and populate the match overview page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Tennis Graphs' });
+router.get('/', function(req, res, next) {
+	//fetch match data
+	var url = 'mongodb://localhost:27017/Tennis';
+	MongoClient.connect(url, function(err, db){
+		if (err){
+			next(err);
+		}
+		var collection = db.collection('games');
+		var matches = collection.find().sort({'$natural':-1}).limit(10).toArray(function(err, matches){
+			res.render('index', { 	
+				title: 'Tennis Graphs',
+				matches: matches,
+			});
+
+		})
+	})
+
+
+
 });
 
 module.exports = router;
