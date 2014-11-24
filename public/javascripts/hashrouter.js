7ac0bf6a2e21fad2c398a07053a3829da7d0eedd
benmiller313 <benmiller313@gmail.com>
2014-11-23 21:58:38 -0700

    hashRouter is basically working. will match a path and call view function, with paramater.

--- public/javascripts/hashrouter.js
+++ public/javascripts/hashrouter.js
@@ -11,11 +11,17 @@ var HashRouter = function(){
 }
 
 HashRouter.prototype.addRoute = function(pattern, view, name) {
-	this.routes.append(new Route(pattern, name, view));
+	this.routes.push(new Route(pattern, view, name));
 };
 
-HashRouter.prototype.direct = function(url_hash) {
-
+HashRouter.prototype.direct = function(hash_path) {
+	for (var i = 0; i < this.routes.length; i++) {
+		matching = hash_path.match(this.routes[i].pattern);
+		if (matching!=null){
+			matching.shift();
+			return this.routes[i].view.apply(null, matching);
+		}
+	}
 };
 
 
--- test/index_router_test.js
+++ test/index_router_test.js
@@ -7,13 +7,35 @@ describe('Hash Router', function(){
 			var route = new HashRouter();
 		});
 	});
-	describe('Add route to router', function(){
+	describe('#addRoute', function(){
 		it('should have a route with name "test" and pattern "test/"', function(){
 			var router = new HashRouter();
-			router.addRoute(/test\//, 'test', function(){return;});
+			router.addRoute(/test\//, function(){return;}, 'test');
 			assert.equal(1, router.routes.length);
 			assert.equal("test", router.routes[0].name);
-			assert.equal(/test\//, router.routes[0].pattern);
+			console.log(router.routes)
+			assert.equal(toString(/test\//), toString(router.routes[0].pattern));
 		});
-	})
+		it('Routes maintain ordering when new route added', function(){
+			var router = new HashRouter();
+			router.addRoute(/test\//, function(){return;}, 'test');
+			router.addRoute(/test2\//, function(){return;}, 'test2');
+			assert.equal(2, router.routes.length);
+			assert.equal("test", router.routes[0].name);
+			assert.equal("test2", router.routes[1].name);
+		})
+	});
+	describe('#direct', function(){
+		it('should match simple string and direct to function', function(){
+			var router = new HashRouter();
+			router.addRoute(/test123/, function(){return "A-OK";});
+			assert.equal('A-OK', router.direct('test123'));
+		});
+		it('should match regex and pass match to view', function(){
+			var router = new HashRouter();
+			router.addRoute(/test\/(\w+)/, function(param){return param;});
+			assert.equal('good', router.direct('test/good'));
+		});
+
+	});
 });
